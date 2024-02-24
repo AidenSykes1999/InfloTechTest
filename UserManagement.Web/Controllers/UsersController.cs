a2bd7fa6c55a12d37b1a0edf63d6b4ad20117e95
Aiden Sykes <aidensykes1999@gmail.com>
2024-02-24 21:16:00 +0000

    Implemented  1. Filters Section

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -11,16 +11,37 @@ public class UsersController : Controller
     public UsersController(IUserService userService) => _userService = userService;
 
     [HttpGet]
-    public ViewResult List()
+    public ViewResult List(bool? active)
+
+        //Since we want either to filter by active, inactive or 'all', the best way to do this is to check if the HTTP has 'active' as a value and if it is true or false. If not, it shall show 'all' instead.
+
     {
-        var items = _userService.GetAll().Select(p => new UserListItemViewModel
+        IEnumerable<UserListItemViewModel> items;
+
+        if (active.HasValue)
         {
-            Id = p.Id,
-            Forename = p.Forename,
-            Surname = p.Surname,
-            Email = p.Email,
-            IsActive = p.IsActive
-        });
+            items = _userService.FilterByActive(active.Value)
+                .Select(p => new UserListItemViewModel
+                {
+                    Id = p.Id,
+                    Forename = p.Forename,
+                    Surname = p.Surname,
+                    Email = p.Email,
+                    IsActive = p.IsActive
+                });
+        }
+        else
+        {
+            items = _userService.GetAll()
+                .Select(p => new UserListItemViewModel
+                {
+                    Id = p.Id,
+                    Forename = p.Forename,
+                    Surname = p.Surname,
+                    Email = p.Email,
+                    IsActive = p.IsActive
+                });
+        }
 
         var model = new UserListViewModel
         {
